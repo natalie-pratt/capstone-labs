2d7421597b09250d708bb013815c55508b6c5e24
Natalie Pratt <nkpratt04@gmail.com>
2023-01-09 19:20:10 -0600

    - classlist finished

---
+++ Week1/list_of_classes.py
@@ -0,0 +1,14 @@
+
+print('\nCreate a list of classes you\'re taking this semester!')
+
+user_classes = [] # List of classes
+
+user_class = input('\nEnter a class, or press enter if you are done: ')
+
+while user_class: # Loop until user presses enter (rendering user_class False)
+    user_classes.append(user_class)
+    user_class = input('\nEnter a class, or press enter if you are done: ')
+
+print('\nYou are taking:')
+for c in user_classes: # Print all classes
+    print(f'\n{c}')
