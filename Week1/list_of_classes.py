8ef9fe7cc6bed690d34862e744ed86ccaa5522b1
Natalie Pratt <nkpratt04@gmail.com>
2023-01-10 12:08:26 -0600

    - More code cleanup, functions added

--- Week1/list_of_classes.py
+++ Week1/list_of_classes.py
@@ -1,16 +1,26 @@
 """This program asks a user to add classes they are taking this semester to a list.
     It will then print out the classes they have written."""
 
-print('\nCreate a list of classes you\'re taking this semester!')
+def main():
+    print('\nCreate a list of classes you\'re taking this semester!')
 
-user_classes = [] # List of classes
+    user_classes = [] # List of classes
 
-user_class = input('\nEnter a class, or press enter if you are done: ')
+    # Call functions to get input from user and then print all input in a list
+    input_user_classes(user_classes) 
+    print_class_list(user_classes)
 
-while user_class: # Loop until user presses enter (rendering user_class False)
-    user_classes.append(user_class)
+# Function allows user to enter however many classes they are taking this semester
+def input_user_classes(user_classes):
     user_class = input('\nEnter a class, or press enter if you are done: ')
+    while user_class: # Loop until user presses enter (rendering user_class False)
+        user_classes.append(user_class)
+        user_class = input('\nEnter a class, or press enter if you are done: ')
 
-print('\nYou are taking:')
-for c in user_classes: # Print all classes
-    print(f'\n{c}')
+# Function to print all classes in classlist
+def print_class_list(user_classes): 
+    print('\nYou are taking:')
+    for c in user_classes: # Print all classes
+        print(f'\n{c}')
+
+main()
