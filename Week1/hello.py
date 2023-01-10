3051e3fb45656239f9b31ca9e2caca2ca64c3ae7
Natalie Pratt <nkpratt04@gmail.com>
2023-01-10 11:54:00 -0600

    - Put code into functions to cleanup

--- Week1/hello.py
+++ Week1/hello.py
@@ -1,16 +1,33 @@
 from datetime import datetime
 
-user_name = input('\nWhat\'s your name? ')
-user_birth_month = input('\nGreat! What month were you born? ')
-# Get current month, spelled out
-current_month = datetime.now().strftime("%B") # Taken from https://www.itsolutionstuff.com/post/how-to-get-current-month-in-pythonexample.html#:~:text=main.py-,from%20datetime%20import%20datetime%20today%20%3D%20datetime.,%2C%20month1)%3B%20month2%20%3D%20today.
+def main():
 
-print(f'\nHello, {user_name}!')
+    user_name, user_birth_month = get_user_input() # Get return values from function
 
-num_letters_in_name = len(user_name)
+    print(f'\nHello, {user_name}!')
+    num_letters_in_name = len(user_name)
+    print(f'\nYou have {num_letters_in_name} letters in your name.')
 
-print(f'\nYou have {num_letters_in_name} letters in your name.')
+    current_month = get_current_month() # Get current month from function
+    is_birthday_month = compare_months(user_birth_month, current_month) # Call function to compare current month with input from user
 
-if user_birth_month == current_month:
-    print('Happy birthday month!')
+    if is_birthday_month: # If current month and user birthday month are the same, print message
+        print('\nHappy birthday month!')
 
+def get_current_month():
+    # Get current month, spelled out
+    current_month = datetime.now().strftime("%B") # Taken from https://www.itsolutionstuff.com/post/how-to-get-current-month-in-pythonexample.html#:~:text=main.py-,from%20datetime%20import%20datetime%20today%20%3D%20datetime.,%2C%20month1)%3B%20month2%20%3D%20today.
+    return current_month
+
+def get_user_input(): # Ask user for name and birthday month
+    user_name = input('\nWhat\'s your name? ')
+    user_birth_month = input('\nGreat! What month were you born? ')
+    return user_name, user_birth_month
+
+def compare_months(user_birth_month, current_month): # Compare user birthday month to current month, case-insensitively
+    if user_birth_month.lower() == current_month.lower():
+        return True
+    else:
+        return False
+
+main()
